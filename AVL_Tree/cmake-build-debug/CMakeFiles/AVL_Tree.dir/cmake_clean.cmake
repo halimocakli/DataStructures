331b1c52220e8dc233d8feab1b4296d8f9719ece
halimocakli <halimcanocakli@gmail.com>
2021-09-11 12:59:16 +0300

    Create cmake_clean.cmake

---
+++ AVL_Tree/cmake-build-debug/CMakeFiles/AVL_Tree.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "AVL_Tree.exe"
+  "AVL_Tree.exe.manifest"
+  "AVL_Tree.pdb"
+  "CMakeFiles/AVL_Tree.dir/main.c.obj"
+  "libAVL_Tree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AVL_Tree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
