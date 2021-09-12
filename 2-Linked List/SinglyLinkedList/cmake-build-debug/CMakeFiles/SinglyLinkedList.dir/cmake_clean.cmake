c30f019541db4f57a9d720ec4d13faa31eff3d88
halimocakli <halimcanocakli@gmail.com>
2021-09-12 03:42:43 +0300

    Create cmake_clean.cmake

---
+++ 2-Linked List/SinglyLinkedList/cmake-build-debug/CMakeFiles/SinglyLinkedList.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SinglyLinkedList.dir/main.c.obj"
+  "SinglyLinkedList.exe"
+  "SinglyLinkedList.exe.manifest"
+  "SinglyLinkedList.pdb"
+  "libSinglyLinkedList.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SinglyLinkedList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
