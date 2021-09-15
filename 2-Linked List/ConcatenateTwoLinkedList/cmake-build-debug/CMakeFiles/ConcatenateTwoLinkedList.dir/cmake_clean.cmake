8d4c421a1e21181c63d068e7972433b576c7a8b2
halimocakli <halimcanocakli@gmail.com>
2021-09-15 14:08:53 +0300

    Create cmake_clean.cmake

---
+++ 2-Linked List/ConcatenateTwoLinkedList/cmake-build-debug/CMakeFiles/ConcatenateTwoLinkedList.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ConcatenateTwoLinkedList.dir/main.c.obj"
+  "ConcatenateTwoLinkedList.exe"
+  "ConcatenateTwoLinkedList.exe.manifest"
+  "ConcatenateTwoLinkedList.pdb"
+  "libConcatenateTwoLinkedList.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ConcatenateTwoLinkedList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
