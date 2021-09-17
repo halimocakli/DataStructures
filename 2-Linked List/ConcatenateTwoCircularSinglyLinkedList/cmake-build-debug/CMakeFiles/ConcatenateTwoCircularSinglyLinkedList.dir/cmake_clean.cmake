c2457b84de03de9d6131e56ee8a37b649e5239bb
halimocakli <halimcanocakli@gmail.com>
2021-09-17 03:58:03 +0300

    Create cmake_clean.cmake

---
+++ 2-Linked List/ConcatenateTwoCircularSinglyLinkedList/cmake-build-debug/CMakeFiles/ConcatenateTwoCircularSinglyLinkedList.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ConcatenateTwoCircularSinglyLinkedList.dir/main.c.obj"
+  "ConcatenateTwoCircularSinglyLinkedList.exe"
+  "ConcatenateTwoCircularSinglyLinkedList.exe.manifest"
+  "ConcatenateTwoCircularSinglyLinkedList.pdb"
+  "libConcatenateTwoCircularSinglyLinkedList.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ConcatenateTwoCircularSinglyLinkedList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
