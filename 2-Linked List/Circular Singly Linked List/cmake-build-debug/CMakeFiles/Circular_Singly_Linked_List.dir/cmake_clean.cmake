50e189d6efd48b48603bcae38c3c58ee6d177a1d
halimocakli <halimcanocakli@gmail.com>
2021-09-18 00:58:36 +0300

    Create cmake_clean.cmake

---
+++ 2-Linked List/Circular Singly Linked List/cmake-build-debug/CMakeFiles/Circular_Singly_Linked_List.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Circular_Singly_Linked_List.dir/main.c.obj"
+  "Circular_Singly_Linked_List.exe"
+  "Circular_Singly_Linked_List.exe.manifest"
+  "Circular_Singly_Linked_List.pdb"
+  "libCircular_Singly_Linked_List.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Circular_Singly_Linked_List.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
