95d6878d1ce95f038db645b9c750f97648d94a9c
halimocakli <halimcanocakli@gmail.com>
2021-09-19 20:00:32 +0300

    Create cmake_clean.cmake

---
+++ 2-Linked List/Circular Doubly Linked List/cmake-build-debug/CMakeFiles/Circular_Doubly_Linked_List.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Circular_Doubly_Linked_List.dir/main.c.obj"
+  "Circular_Doubly_Linked_List.exe"
+  "Circular_Doubly_Linked_List.exe.manifest"
+  "Circular_Doubly_Linked_List.pdb"
+  "libCircular_Doubly_Linked_List.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Circular_Doubly_Linked_List.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
