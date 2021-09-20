1615397949008d2ecd1766bbc9830facc9fee70a
halimocakli <halimcanocakli@gmail.com>
2021-09-21 02:03:05 +0300

    Create cmake_clean.cmake

---
+++ 4-Queue/PalindromeImplementationByUsingStackAndQueue/cmake-build-debug/CMakeFiles/PalindromeImplementationByUsingStackAndQueue.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PalindromeImplementationByUsingStackAndQueue.dir/main.c.obj"
+  "PalindromeImplementationByUsingStackAndQueue.exe"
+  "PalindromeImplementationByUsingStackAndQueue.exe.manifest"
+  "PalindromeImplementationByUsingStackAndQueue.pdb"
+  "libPalindromeImplementationByUsingStackAndQueue.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PalindromeImplementationByUsingStackAndQueue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
