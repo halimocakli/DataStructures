dd5b8ffb13a21b0c1fb36f7f2d507ede9e52b9fd
halimocakli <halimcanocakli@gmail.com>
2021-09-20 03:17:28 +0300

    Create cmake_clean.cmake

---
+++ 4-Queue/CircularQueueImplementationByUsingArray/cmake-build-debug/CMakeFiles/CircularQueueImplementationByUsingArray.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CircularQueueImplementationByUsingArray.dir/main.c.obj"
+  "CircularQueueImplementationByUsingArray.exe"
+  "CircularQueueImplementationByUsingArray.exe.manifest"
+  "CircularQueueImplementationByUsingArray.pdb"
+  "libCircularQueueImplementationByUsingArray.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CircularQueueImplementationByUsingArray.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
