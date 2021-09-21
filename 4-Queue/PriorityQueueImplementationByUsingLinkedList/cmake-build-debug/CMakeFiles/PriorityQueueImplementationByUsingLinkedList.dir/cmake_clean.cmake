c76397fc6856f1ce509a4084c46232775f56c1c1
halimocakli <halimcanocakli@gmail.com>
2021-09-22 00:47:12 +0300

    Create cmake_clean.cmake

---
+++ 4-Queue/PriorityQueueImplementationByUsingLinkedList/cmake-build-debug/CMakeFiles/PriorityQueueImplementationByUsingLinkedList.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PriorityQueueImplementationByUsingLinkedList.dir/main.c.obj"
+  "PriorityQueueImplementationByUsingLinkedList.exe"
+  "PriorityQueueImplementationByUsingLinkedList.exe.manifest"
+  "PriorityQueueImplementationByUsingLinkedList.pdb"
+  "libPriorityQueueImplementationByUsingLinkedList.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PriorityQueueImplementationByUsingLinkedList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
