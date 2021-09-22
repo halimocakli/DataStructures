a15c1f35a2e266672805b20716b40ab9d150dded
halimocakli <halimcanocakli@gmail.com>
2021-09-23 00:24:46 +0300

    Create cmake_clean.cmake

---
+++ 4-Queue/QueueImplementationByUsingArray/cmake-build-debug/CMakeFiles/QueueImplementationByUsingArray.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QueueImplementationByUsingArray.dir/main.c.obj"
+  "QueueImplementationByUsingArray.exe"
+  "QueueImplementationByUsingArray.exe.manifest"
+  "QueueImplementationByUsingArray.pdb"
+  "libQueueImplementationByUsingArray.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/QueueImplementationByUsingArray.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
