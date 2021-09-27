6d2cdfc401d71f31fd1f04a29ff44b68148fff40
halimocakli <halimcanocakli@gmail.com>
2021-09-27 14:41:53 +0300

    Create cmake_clean.cmake

---
+++ 4-Queue/QueueImplementationByUsingLinkedList_1/cmake-build-debug/CMakeFiles/QueueImplementationByUsingLinkedList_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QueueImplementationByUsingLinkedList_1.dir/main.c.obj"
+  "QueueImplementationByUsingLinkedList_1.exe"
+  "QueueImplementationByUsingLinkedList_1.exe.manifest"
+  "QueueImplementationByUsingLinkedList_1.pdb"
+  "libQueueImplementationByUsingLinkedList_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/QueueImplementationByUsingLinkedList_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
