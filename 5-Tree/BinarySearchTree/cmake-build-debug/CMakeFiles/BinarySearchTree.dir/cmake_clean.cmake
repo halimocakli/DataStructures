a04a97ec06306dd9dbef75c60e4f64e64ce233f0
halimocakli <halimcanocakli@gmail.com>
2021-10-01 00:01:48 +0300

    Create cmake_clean.cmake

---
+++ 5-Tree/BinarySearchTree/cmake-build-debug/CMakeFiles/BinarySearchTree.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BinarySearchTree.exe"
+  "BinarySearchTree.exe.manifest"
+  "BinarySearchTree.pdb"
+  "CMakeFiles/BinarySearchTree.dir/main.c.obj"
+  "libBinarySearchTree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BinarySearchTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
