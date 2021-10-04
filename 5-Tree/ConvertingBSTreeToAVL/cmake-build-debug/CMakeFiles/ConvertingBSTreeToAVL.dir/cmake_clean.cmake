bbe86b4c3164caad25f7704c6d2223e2e986e978
halimocakli <halimcanocakli@gmail.com>
2021-10-04 03:48:26 +0300

    Create cmake_clean.cmake

---
+++ 5-Tree/ConvertingBSTreeToAVL/cmake-build-debug/CMakeFiles/ConvertingBSTreeToAVL.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ConvertingBSTreeToAVL.dir/main.c.obj"
+  "ConvertingBSTreeToAVL.exe"
+  "ConvertingBSTreeToAVL.exe.manifest"
+  "ConvertingBSTreeToAVL.pdb"
+  "libConvertingBSTreeToAVL.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ConvertingBSTreeToAVL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
