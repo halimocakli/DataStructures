28fa65cdaed8e98da099c4194172a95e552d37a8
halimocakli <halimcanocakli@gmail.com>
2021-10-05 02:27:37 +0300

    Create cmake_clean.cmake

---
+++ 3-Stack/InfixToPostfixConvertingImplementationByUsingStack/cmake-build-debug/CMakeFiles/InfixToPostfixConvertingImplementationByUsingStack.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/InfixToPostfixConvertingImplementationByUsingStack.dir/main.c.obj"
+  "InfixToPostfixConvertingImplementationByUsingStack.exe"
+  "InfixToPostfixConvertingImplementationByUsingStack.exe.manifest"
+  "InfixToPostfixConvertingImplementationByUsingStack.pdb"
+  "libInfixToPostfixConvertingImplementationByUsingStack.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/InfixToPostfixConvertingImplementationByUsingStack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
