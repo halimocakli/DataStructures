c557d64aa8042b2172c63d10135c279bbf4b6cd7
halimocakli <halimcanocakli@gmail.com>
2021-10-06 02:23:11 +0300

    Create cmake_clean.cmake

---
+++ 3-Stack/PostfixEvaluationByUsingStack/cmake-build-debug/CMakeFiles/PostfixEvaluationByUsingStack.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PostfixEvaluationByUsingStack.dir/main.c.obj"
+  "PostfixEvaluationByUsingStack.exe"
+  "PostfixEvaluationByUsingStack.exe.manifest"
+  "PostfixEvaluationByUsingStack.pdb"
+  "libPostfixEvaluationByUsingStack.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PostfixEvaluationByUsingStack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
