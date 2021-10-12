00405d4e8e4ce26ca4f7aed043a4eab80e8e4a72
halimocakli <halimcanocakli@gmail.com>
2021-10-12 11:35:40 +0300

    Create cmake_clean.cmake

---
+++ 3-Stack/StackImplementationByUsingArray2/cmake-build-debug/CMakeFiles/StackImplementationByUsingArray2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StackImplementationByUsingArray2.dir/main.c.obj"
+  "StackImplementationByUsingArray2.exe"
+  "StackImplementationByUsingArray2.exe.manifest"
+  "StackImplementationByUsingArray2.pdb"
+  "libStackImplementationByUsingArray2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/StackImplementationByUsingArray2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
