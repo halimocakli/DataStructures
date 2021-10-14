65810de35b220a0fd21577134977d5c086767de5
halimocakli <halimcanocakli@gmail.com>
2021-10-14 03:18:10 +0300

    Create cmake_clean.cmake

---
+++ 3-Stack/StackImplementationByUsingLinkedList/cmake-build-debug/CMakeFiles/StackImplementationByUsingLinkedList.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StackImplementationByUsingLinkedList.dir/main.c.obj"
+  "StackImplementationByUsingLinkedList.exe"
+  "StackImplementationByUsingLinkedList.exe.manifest"
+  "StackImplementationByUsingLinkedList.pdb"
+  "libStackImplementationByUsingLinkedList.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/StackImplementationByUsingLinkedList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
