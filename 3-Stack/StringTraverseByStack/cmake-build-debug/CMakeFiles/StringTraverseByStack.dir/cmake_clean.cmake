8a2781767b66f98c0cd58bda427142beef3ac867
halimocakli <halimcanocakli@gmail.com>
2021-10-15 10:40:59 +0300

    Create cmake_clean.cmake

---
+++ 3-Stack/StringTraverseByStack/cmake-build-debug/CMakeFiles/StringTraverseByStack.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StringTraverseByStack.dir/main.c.obj"
+  "StringTraverseByStack.exe"
+  "StringTraverseByStack.exe.manifest"
+  "StringTraverseByStack.pdb"
+  "libStringTraverseByStack.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/StringTraverseByStack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
