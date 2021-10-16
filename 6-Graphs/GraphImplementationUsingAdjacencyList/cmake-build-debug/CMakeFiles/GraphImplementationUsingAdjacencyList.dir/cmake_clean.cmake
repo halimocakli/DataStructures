f5913df69e2f001f556af100ee517e40e55bbd45
halimocakli <halimcanocakli@gmail.com>
2021-10-16 11:40:58 +0300

    Create cmake_clean.cmake

---
+++ 6-Graphs/GraphImplementationUsingAdjacencyList/cmake-build-debug/CMakeFiles/GraphImplementationUsingAdjacencyList.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GraphImplementationUsingAdjacencyList.dir/main.c.obj"
+  "GraphImplementationUsingAdjacencyList.exe"
+  "GraphImplementationUsingAdjacencyList.exe.manifest"
+  "GraphImplementationUsingAdjacencyList.pdb"
+  "libGraphImplementationUsingAdjacencyList.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/GraphImplementationUsingAdjacencyList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
