435ce178fbbe08b186bb4ea3651e8a52470610a7
halimocakli <halimcanocakli@gmail.com>
2021-10-19 00:04:53 +0300

    Create cmake_clean.cmake

---
+++ 7- Heap/HeapImplementationUsingPriorityQueue/cmake-build-debug/CMakeFiles/HeapImplementationUsingPriorityQueue.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HeapImplementationUsingPriorityQueue.dir/main.c.obj"
+  "HeapImplementationUsingPriorityQueue.exe"
+  "HeapImplementationUsingPriorityQueue.exe.manifest"
+  "HeapImplementationUsingPriorityQueue.pdb"
+  "libHeapImplementationUsingPriorityQueue.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HeapImplementationUsingPriorityQueue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
