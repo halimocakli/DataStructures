877c91c59096e52bb8c2eba855a942f38484b907
halimocakli <halimcanocakli@gmail.com>
2021-11-07 00:28:02 +0300

    Create cmake_clean.cmake

---
+++ 2-Linked List/Adding Numbers to Linked List/cmake-build-debug/CMakeFiles/Adding_Numbers_to_Linked_List.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Adding_Numbers_to_Linked_List.exe"
+  "Adding_Numbers_to_Linked_List.exe.manifest"
+  "Adding_Numbers_to_Linked_List.pdb"
+  "CMakeFiles/Adding_Numbers_to_Linked_List.dir/main.c.obj"
+  "libAdding_Numbers_to_Linked_List.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Adding_Numbers_to_Linked_List.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
